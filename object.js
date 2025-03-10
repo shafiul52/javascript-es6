75648a2dcc731f646f3a3cfc4fcf2f6714a74932
Your Name <shafiulhossain52@gmail.com>
2025-03-10 15:08:31 +0600

    object function done

---
+++ object.js
@@ -0,0 +1,62 @@
+// const myInfo = {
+//     name : "shafiul Alam",
+//     age : 27,
+//     address: "Netrakona",
+//     sosourName:"Abdul Motalib",
+//     sosurBari: "B.baria",
+//     contact:{
+//         phone:264646,
+//         email:"shafiulhossain52@gmail.com"
+//     }
+    
+   
+// };
+
+
+// const obj2 = {...myInfo};
+
+
+// obj2.married = false;
+// obj2.age = 26;
+// console.log(myInfo);
+// console.log(obj2);
+
+
+// const myname = myInfo.name;
+// const myage = myInfo.name;
+
+// const {age:myAge} = myInfo
+// console.log(myAge);
+
+// const {contact:{email}} = myInfo;
+// console.log(contact);
+// const{email} = contact;
+// console.log(email)
+
+const myInfo = {
+    name : "shafiul Alam",
+    age : 27,
+    address: "Netrakona",
+    sosourName:"Abdul Motalib",
+    sosurBari: "B.baria",
+    // contact:{
+    //     phone:264646,
+    //     email:"shafiulhossain52@gmail.com"
+    // },
+    
+   
+};
+
+// for (let key  in myInfo){
+//     console.log(myInfo[key])
+// }
+
+
+// object acces: dot notation, bracket notation
+
+const x = "name"
+const s = "sosourName"
+
+// console.log(myInfo.sosourName)
+// console.log(myInfo["sosourName"])
+// console.log(myInfo[s])
