e250728ea60bcfe3dc45da7fef6d6ea594c832f8
Your Name <shafiulhossain52@gmail.com>
2025-03-10 07:12:03 +0600

    arrayspreading done

---
+++ arraySpread.js
@@ -0,0 +1,34 @@
+const  arr1 = [1,2,3];
+
+// const  arr2  = [...arr1];
+const  arr2 = []
+
+
+
+// for(let item of arr1){
+//     arr2.push(item)
+// }
+
+
+// arr1.push(50);
+// arr1.push(60);
+
+// console.log(arr2);
+
+// arr2.push(66);
+// console.log(arr2)
+// console.log(arr1)
+
+const names = [ " abdul hekim", "Yusuf Ali", "sattar mia", "mojibur Rahman" ]
+
+// const  name1 = names[0];
+// const  name2 = names[1];
+// const name3 = names[2];
+
+// console.log(name2)
+
+const [_,y,z,] = [ " abdul hekim", "Yusuf Ali", "sattar mia", "mojibur Rahman" ];
+
+// console.log(w,x,y,z)
+console.log(z);
+ 
--- logical.js
+++ logical.js
@@ -2,4 +2,17 @@
 
 // const x =5;
 // console.log(!!x)
-const money = true;
+// const money = 50;
+// and && 
+// left && right 
+// console.log(money && "varsity jabo")
+
+
+//  or ||
+
+// const bus  = ""
+// const rickshaw = "rickshaw ace"
+// console.log(bus || rickshaw)
+// const x = 5;
+// statement  ? if true  : false
+// console.log(x === 5 ? "number is 5 " : "not is five ")
